0ded7b16a6a35d85faf1d517a68a523da3ee9d7f
Divya Tiwari <divyatiwari1112@gmail.com>
2018-06-07 23:13:59 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-
+puts("Welcome to Tic Tac Toe!")
+puts("Where would you like to go?")
 def input_to_index(num)
   num.to_i
   board[num-1]="X"
